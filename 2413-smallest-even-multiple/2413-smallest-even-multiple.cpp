9e38cb6a7661329e065116df04058587365dfeb8
kishore_sk <kishores302007@gmail.com>
2026-04-01 10:17:28 +0530

    Time: 0 ms (100%), Space: 7.9 MB (29.66%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -5,7 +5,7 @@ public:
         if(n <= 0){
             return 0;
         }
-        //check odd or even
+        //check odd or even and return 
         if((n % 2) == 0){
             return n;
         }

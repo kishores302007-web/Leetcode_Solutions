3d593f363df2ecf80256e98b80b6bd209f891610
kishore_sk <kishores302007@gmail.com>
2026-07-16 11:39:53 +0530

    Time: 0 ms (100%), Space: 21.8 MB (71.03%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+      int right = nums.size();
+      int left = 0;  
+      sort(nums.begin(),nums.end());
+      while(left < right){
+        int mid = left + (right - left) / 2;
+        if(nums[mid] > mid){
+            right = mid;
+        }
+        else{
+            left = mid + 1;
+        }
+      }
+        return left;
+      
+    }
+};

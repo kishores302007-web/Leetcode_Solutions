bf554990419d0991e84c5d3f613d7e2f98415abf
kishore_sk <kishores302007@gmail.com>
2026-07-24 11:37:38 +0530

    Time: 0 ms (100%), Space: 17.5 MB (54.66%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,41 +1,50 @@
 class Solution {
-public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int first = -1;int last = -1;
-        int left = 0; int right = nums.size()-1;
-
+int firstindex(vector < int>& nums,int target){
 // to find initial index of starting element
-
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
-                first = mid;
-                right = mid - 1;
-            }
-            else if(nums[mid] > target){
-                right = mid - 1;
-            }
-            else{
-                left = mid + 1;
-            }
+    int first = -1;
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            first = mid;
+            right = mid - 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
         }
+    }
+    return first;
+} 
 
+int lastindex(vector < int>& nums, int target) {
 // to find the last index of the target
-
-        left = 0; right = nums.size() - 1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
-                last = mid;
-                left = mid + 1;
-            }
-            else if(nums[mid] > target){
-                right = mid - 1;
-            }
-            else{
-                left = mid + 1;
-            }
+    int last = -1;
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            last = mid;
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
         }
-        return {first,last};
+    }
+    return last;
+}
+public:
+    vector<int> searchRange(vector<int>& nums, int target){
+        vector<int> ans;
+        ans.push_back(firstindex(nums,target));
+        ans.push_back(lastindex(nums,target));
+        return ans;
     }
 };
+
+
+
+  

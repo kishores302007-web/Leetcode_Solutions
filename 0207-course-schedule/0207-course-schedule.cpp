08adfa6fae94054555092b5ce900bbc83aa143f1
kishore_sk <kishores302007@gmail.com>
2026-08-18 15:55:01 +0530

    Time: 3 ms (75.5%), Space: 20.1 MB (24.55%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        for (auto p : prerequisites) {
+            int course = p[0];
+            int prerequisite = p[1];
+
+            graph[prerequisite].push_back(course);
+            indegree[course]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int count = 0;
+
+        while (!q.empty()) {
+
+            int current = q.front();
+            q.pop();
+
+            count++;
+
+            for (int next : graph[current]) {
+
+                indegree[next]--;
+
+                if (indegree[next] == 0) {
+                    q.push(next);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+};

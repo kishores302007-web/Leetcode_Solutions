c867a6aad7beff0698ef4d38b844f459c7611939
kishore_sk <kishores302007@gmail.com>
2026-09-02 13:51:37 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-   if (s.length() != t.length()) return false;
-
-   vector<int> count (26,0);
-   for (int i=0;i<s.length();i++){
-        count[s[i] - 'a']++;
-        count[t[i] - 'a']--;
-    } 
-    for (int c : count){
-        if(c != 0) return false;
-    }
-    return true;
-
+        unordered_map<char,int> map;
+        if(s.size() != t.size()){
+            return false;
+        }
+        for(int i = 0; i<s.size();i++){
+            map[s[i]]++;
+        }
+        for(int i = 0; i<t.size();i++){
+            map[t[i]]--;
+            if(map[t[i]] < 0){
+                return false;
+            }
+        }
+        return true;
     }
 };

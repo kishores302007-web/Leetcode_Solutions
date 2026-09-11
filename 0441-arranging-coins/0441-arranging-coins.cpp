99b377c6bee43ec5514355e5e4bc8182e895aad3
kishore_sk <kishores302007@gmail.com>
2026-09-11 14:34:44 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.72%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long left = 1;
+        long long right = n;
+        while (right >= left) {
+            long long mid = left + (right - left) / 2;
+            long long coin = mid * (mid + 1) / 2;
+            if (coin <= n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+};
